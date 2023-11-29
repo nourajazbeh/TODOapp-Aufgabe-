defc4840897513e671a4c5a67727bc123a7f0b07
Mikhail Sarnov <kpblm2@gmail.com>
2023-11-29 21:17:27 +0100

    funktioniert

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
-from typing import Union, Annotated
+from typing import Annotated
 from fastapi import FastAPI, Request, Response, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
-import mysql
 import mysql.connector
 from mysql.connector import Error
 
@@ -16,34 +15,52 @@ DB_HOST = "localhost"
 DB_NAME = "todosdb"
 
 def create_db_connection():
-    connection = mysql.connector.connect(user=DB_USER,password=DB_PASSWORD, host=DB_HOST, database=DB_NAME)
-    return connection
+    try:
+        connection = mysql.connector.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database=DB_NAME)
+        return connection
+    except Error as e:
+        print(f"Error: {e}")
+        raise
 
 @app.get("/", response_class=HTMLResponse)
-def home(request: Request):
-    return templates.TemplateResponse("index.html",
-                                        {"request": request})
-
-@app.get("/todos", response_class=HTMLResponse)
-def get_todos(request: Request, todo: Annotated[str, Form()]):
+async def read_todos(request: Request):
     connection = create_db_connection()
-    cursor = connection.cursor()
-    query = "SELECT item, stat FROM todos;"
+    cursor = connection.cursor(dictionary=True)
+    query = "SELECT * FROM todos;"
     cursor.execute(query)
-    todos = cursor.fetchall()    
-    senderid = todo
-    return templates.TemplateResponse("indexs.html", {"request": request, "todos": todos, "senderid": senderid})
-
+    todos = cursor.fetchall()
+    return templates.TemplateResponse("index.html", {"request": request, "todos": todos})
 
 @app.post("/todos")
-def post_todos(response: Response, todo: Annotated[str, Form()]):
+async def create_todo(todo: str = Form(...)):
     connection = create_db_connection()
     cursor = connection.cursor()
     query = "INSERT INTO todos (item, stat) VALUES (%s, %s)"
     data = (todo, "open")
     cursor.execute(query, data)
     connection.commit()
-    response = RedirectResponse(url="http://localhost:8000/", status_code=302)
-    return response
+    return RedirectResponse(url="/?message=ToDo created successfully", status_code=302)
 
-    print(todo)
+@app.post("/update_status/{todo_id}/{new_status}")
+async def update_status(todo_id: int, new_status: str):
+    connection = create_db_connection()
+    cursor = connection.cursor()
+    query = "UPDATE todos SET stat = %s WHERE id = %s"
+    data = (new_status, todo_id)
+    cursor.execute(query, data)
+    connection.commit()
+    return RedirectResponse(url=f"/?message=Status of ToDo {todo_id} updated to {new_status}", status_code=302)
+
+@app.post("/delete_todo/{todo_id}")
+async def delete_todo(todo_id: int):
+    connection = create_db_connection()
+    cursor = connection.cursor()
+    query = "DELETE FROM todos WHERE id = %s"
+    data = (todo_id,)
+    cursor.execute(query, data)
+    connection.commit()
+    return RedirectResponse(url=f"/?message=ToDo {todo_id} deleted successfully", status_code=302)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
